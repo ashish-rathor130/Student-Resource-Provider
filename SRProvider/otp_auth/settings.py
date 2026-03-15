33994ea0255e263157ee10b1d846d20d7ed53ed6
Ashish Rathor <anvdgroup@gmail.com>
2026-03-15 11:20:30 +0530

    Modify settings for allowed hosts and email backend
    Updated allowed hosts and added email configuration.

--- SRProvider/otp_auth/settings.py
+++ SRProvider/otp_auth/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7of9^7i^$obfpq$wal36a_qfugrrcfv41bnhpgzqtov7b=o7p-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://student-resource-provider.onrender.com']
 
 
 # Application definition
